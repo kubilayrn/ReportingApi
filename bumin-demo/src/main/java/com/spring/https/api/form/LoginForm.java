212e0480f2a295f308f73c9653aa12a25a2beb69
Kubilay Ürün <kubilayurun97@gmail.com>
2018-11-23 02:16:40 +0300

    Added LoginForm class

---
+++ bumin-demo/src/main/java/com/spring/https/api/form/LoginForm.java
@@ -0,0 +1,24 @@
+package com.spring.https.api.form;
+
+public class LoginForm {
+	private String email;
+	private String password;
+	public LoginForm() {
+		super();
+		
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
